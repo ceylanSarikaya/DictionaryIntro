a6ca400eae083b557582e680ff9ce79f6c35d559
Sinan Kara <snan_kara_@hotmail.com>
2021-01-21 19:00:09 +0300

    Add project files.

---
+++ DictionaryIntro/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryIntro
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+       
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value) 
+        {
+            TKey[] tempKeyArray = keys;
+            TValue[] tempValueArray = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keys[i] = tempKeyArray[i];
+                values[i] = tempValueArray[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+    }
+}
---
+++ DictionaryIntro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> cities2 = new MyDictionary<int, string>();
+            cities2.Add(34, "Istanbul");
+            cities2.Add(06, "Ankara");
+            cities2.Add(07, "Antalya");
+          
+        }
+    }
+}
